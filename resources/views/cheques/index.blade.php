93f7215c78ddfe454db20ec06fe502ee0c8fda5d
sebabarr <sebabarr@gmail.com>
2016-11-09 03:50:06 +0000

    fd

--- resources/views/cheques/index.blade.php
+++ resources/views/cheques/index.blade.php
@@ -48,7 +48,7 @@
                     <td>{{ $item->importe-$item->desctasa-$item->descgasto-$item->descfijo }}</td>
                     <td>{{ $item->cuits->razonsocial }}</td>
                     <td>{{ $item->clientes->razonsocial }}</td>
-                    <td><a href="{{ url('cheques', $item->id) }}">{{ $item->nrocheque }}</a></td>
+                    
                     <td>
                         <a href="{{ url('cheques/' . $item->id . '/edit') }}">
                             <button type="submit" class="btn btn-primary btn-xs">Modificar</button>
